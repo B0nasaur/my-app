7d454d777cfb874d8f8c840465afe6cdf9a06189
B0nasaur <vonjulian.capistrano-21@cpu.edu.ph>
2024-07-18 15:07:37 +0800

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex nw-screen text-5xl font-bold min-h-screen flex-col items-center justify-between p-24">
+      Hello my name is Von Julian Capistrano
+    </main>
+  );
+}
